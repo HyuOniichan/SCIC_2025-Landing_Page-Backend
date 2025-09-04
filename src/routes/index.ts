1c399ecf3ef30b3d68aa39f0d4d02edd19c97578
phucanh <phucanh05022004@gmail.com>
2025-09-04 11:46:50 +0700

    fix post's bug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,10 +7,10 @@ import submitRoute from './submission.route'
 function route(app: Application) {
     const prefix = '/api/v1'; 
 
+    app.use(`${prefix}/posts`, postRoute); 
+    app.use(`${prefix}/submissions`, submitRoute);     
     app.use(`${prefix}/user`, userRoute); 
     app.use(`${prefix}/connect`, connectRoute); 
-    app.use(`${prefix}/posts`, postRoute); 
-    app.use(`${prefix}/submissions`, submitRoute); 
 
     app.use(`${prefix}/health`, (req: Request, res: Response) => {
         res.status(200).json({
